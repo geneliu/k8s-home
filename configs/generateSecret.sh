0737a344af34527ca0179680eff2a2cbb096ea82
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2021-01-10 14:24:19 +0100

    Add Cloudflare issuer to cert-manager

--- configs/generateSecret.sh
+++ configs/generateSecret.sh
@@ -25,28 +25,27 @@ fi
 
 generate() {
     MAPPING_FILE="./mapping.json"
-    NAME=`$JQ_COMMAND .mapping.$1.name $MAPPING_FILE`
-    KEY=`$JQ_COMMAND .mapping.$1.key $MAPPING_FILE`
-    VALUES_PATH=`$JQ_COMMAND .mapping.$1.values_path $MAPPING_FILE`
-    SECRET_NAME=`$JQ_COMMAND .mapping.$1.secret_name $MAPPING_FILE`
-    UNSEALED_SECRET_PATH=`$JQ_COMMAND .mapping.$1.unsealed_secret_path $MAPPING_FILE`
-    SEALED_SECRET_PATH=`$JQ_COMMAND .mapping.$1.sealed_secret_path $MAPPING_FILE`
-    NAMESPACE=`$JQ_COMMAND .mapping.$1.namespace $MAPPING_FILE`
+    NAME=`$JQ_COMMAND .mapping[\"$1\"].name $MAPPING_FILE`
+    KEY=`$JQ_COMMAND .mapping[\"$1\"].key $MAPPING_FILE`
+    VALUES_PATH=`$JQ_COMMAND .mapping[\"$1\"].values_path $MAPPING_FILE`
+    SECRET_NAME=`$JQ_COMMAND .mapping[\"$1\"].secret_name $MAPPING_FILE`
+    UNSEALED_SECRET_PATH=`$JQ_COMMAND .mapping[\"$1\"].unsealed_secret_path $MAPPING_FILE`
+    SEALED_SECRET_PATH=`$JQ_COMMAND .mapping[\"$1\"].sealed_secret_path $MAPPING_FILE`
+    NAMESPACE=`$JQ_COMMAND .mapping[\"$1\"].namespace $MAPPING_FILE`
     PUBLIC_KEY_PATH=`$JQ_COMMAND .public_key_path $MAPPING_FILE`
     echo "Generating secrets for $NAME..."
     echo "================================================================"
     echo "Checking for values file..."
     if ! [[ -f $VALUES_PATH ]]; then
-        echo "Values file at $VALUES_PATH was not found."
-        exit 1
+        echo "Values file at $VALUES_PATH was not found. Assuming it's a plain Secret stored in unsealed-secrets."
     else
         echo "Values file at $VALUES_PATH was found."
         echo "================================================================"
         echo "Generating unsealed secret..."
+        kubectl create secret generic $SECRET_NAME --from-file="$KEY=$VALUES_PATH" --namespace="$NAMESPACE" --dry-run=client -o yaml > $UNSEALED_SECRET_PATH
+        echo "Secret $SECRET_NAME has been outputted to $UNSEALED_SECRET_PATH."
+        echo "================================================================"
     fi
-    kubectl create secret generic $SECRET_NAME --from-file="$KEY=$VALUES_PATH" --namespace="$NAMESPACE" --dry-run=client -o yaml > $UNSEALED_SECRET_PATH
-    echo "Secret $SECRET_NAME has been outputted to $UNSEALED_SECRET_PATH."
-    echo "================================================================"
     echo "Generating sealed secret..."
     kubeseal --format=yaml --cert=$PUBLIC_KEY_PATH --namespace="$NAMESPACE" < $UNSEALED_SECRET_PATH > $SEALED_SECRET_PATH
     echo "Sealed secret $SECRET_NAME has been outputted to $SEALED_SECRET_PATH."
